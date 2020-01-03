09ac739e5ae99dfff8d89a4007c8bbc46b86a064
cesardramirez <cesardavid89@gmail.com>
2020-01-02 21:28:05 -0500

    feat(initial): Initial commit.

---
+++ src/test/java/com/platzi/ereservation/EReservationApplicationTests.java
@@ -0,0 +1,12 @@
+package com.platzi.ereservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EReservationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
