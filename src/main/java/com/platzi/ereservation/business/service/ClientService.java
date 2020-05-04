3c3afae5a6eabbff612810576b0e6caa99a3b736
cesardramirez <cesardavid89@gmail.com>
2020-05-03 19:23:04 -0500

    feat(ann): Add Transactional annotation.

--- src/main/java/com/platzi/ereservation/business/service/ClientService.java
+++ src/main/java/com/platzi/ereservation/business/service/ClientService.java
@@ -3,6 +3,8 @@ package com.platzi.ereservation.business.service;
 import com.platzi.ereservation.business.repository.ClientRepository;
 import com.platzi.ereservation.models.Client;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 /**
  * Class that define services (operations) of the Client class.
  *
@@ -10,8 +12,16 @@ import org.springframework.stereotype.Service;
  *
  * En CrudRepository.class existen los métodos de save(), findById(), deleteById(), etc.
  * Estos serán los servicios que serán llamados y utilizados por el Front-end.
+ *
+ * @Transactional : Si se define a nivel de clase, todos los métodos serán tratados como transaccionales
+ *   (que afectan a la base de datos). Si se le añade <p>readOnly = true</p> será tratado únicamente como modo lectura.
+ *
+ * rollbackFor = Exception.class : Cualquier método que arroje una excepción no verificada (RuntimeException o Error)
+ *   activará el rollback automáticamente, pero, cualquier excepción de verificación no activará el rollback a menos
+ *   que se le indique.
  */
 @Service
+//@Transactional(readOnly = true, rollbackFor = Exception.class)
 public class ClientService {
 
     private final ClientRepository clientRepository;
@@ -25,6 +35,7 @@ public class ClientService {
      * @param client
      * @return Client
      */
+    @Transactional
     public Client create(Client client) {
         return clientRepository.save(client);
     }
@@ -34,6 +45,7 @@ public class ClientService {
      * @param client
      * @return Client
      */
+    @Transactional
     public Client update(Client client) {
         return clientRepository.save(client);
     }
@@ -42,6 +54,7 @@ public class ClientService {
      * Service to delete a client.
      * @param client
      */
+    @Transactional
     public void delete(Client client) {
         clientRepository.delete(client);
     }
@@ -50,6 +63,7 @@ public class ClientService {
      * Service to find a client by ID.
      * @param identification
      */
+    @Transactional(readOnly = true)
     public Client findByIdentification(String identification) {
         return clientRepository.findByIdentification(identification);
     }
